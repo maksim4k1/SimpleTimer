c2e7c2987f39baca46fb5739d7c3ecef56952ad3
Maksim Bazhenov <maksim4k1@gmail.com>
2021-09-01 13:06:53 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import ToggleDay from './components/ToggleDay';
+import "./App.css";
+import Wrapper from './components/Wrapper' // нужно создать
+import Circle from './components/Circle' // нужно создать
+import { useState } from "react";
 
-function App() {
-    return (
-        <div className="App">
-            <ToggleDay/>
-        </div>
-    );
+export default function App() {
+  const [color, setColor] = useState(0)
+
+  function colorHandler(colorNum){
+    setColor(colorNum);
+  }
+
+  return (
+    <div className={`bg color-${color}`}>
+      <Wrapper>
+        {[1, 2, 3, 4, 5].map((num) => {
+          return (
+            <Circle key={num} num={num} color={color} onChangeColor={colorHandler}/>
+          );
+        })}
+      </Wrapper>
+    </div>
+  );
 }
-
-export default App;
